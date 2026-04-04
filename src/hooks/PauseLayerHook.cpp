007c03120e85c6055d109b030da55ae71d44aaab
hkr3d2 <151843832+hkr3d2@users.noreply.github.com>
2026-04-04 10:18:34 +0200

    Refactor PauseLayer for improved ghost management
    Refactor PauseLayer to use member fields for race toggle and selected ghost label. Update init method to accept unfocused parameter and adjust related logic.

--- src/hooks/PauseLayerHook.cpp
+++ src/hooks/PauseLayerHook.cpp
@@ -1,16 +1,18 @@
 #include "PauseLayerHook.hpp"
 #include "../GhostManager.hpp"
+#include "../ui/GhostLibraryPopup.hpp"
 
-bool PauseLayer::init() {
-    if (!PauseLayer::init()) return false;
+bool PauseLayer::init(bool unfocused) {
+    if (!PauseLayer::init(unfocused)) return false;
     
-    addRaceControls();
+    this->addRaceControls();
     
     return true;
 }
 
 void PauseLayer::addRaceControls() {
     auto menu = CCMenu::create();
+    menu->setPosition({0, 0});
     
     // Create toggle button for race mode
     auto onSprite = CCSprite::createWithSpriteFrameName("GJ_checkOn_001.png");
@@ -22,22 +24,22 @@ void PauseLayer::addRaceControls() {
         this, menu_selector(PauseLayer::onToggleRace)
     );
     toggle->toggle(isRacing);
-    toggle->setPosition(ccp(200, 200));
+    toggle->setPosition({200, 200});
     menu->addChild(toggle);
-    m_raceToggle = toggle;
+    m_fields->m_raceToggle = toggle;
     
     // Label for toggle
     auto toggleLabel = CCLabelBMFont::create("Race Ghost", "bigFont.fnt");
-    toggleLabel->setPosition(ccp(200, 175));
+    toggleLabel->setPosition({200, 175});
     toggleLabel->setScale(0.6f);
     menu->addChild(toggleLabel);
     
     // Show selected ghost
-    updateSelectedGhostLabel();
-    if (m_selectedGhostLabel) {
-        m_selectedGhostLabel->setPosition(ccp(200, 145));
-        m_selectedGhostLabel->setScale(0.5f);
-        menu->addChild(m_selectedGhostLabel);
+    this->updateSelectedGhostLabel();
+    if (m_fields->m_selectedGhostLabel) {
+        m_fields->m_selectedGhostLabel->setPosition({200, 145});
+        m_fields->m_selectedGhostLabel->setScale(0.5f);
+        menu->addChild(m_fields->m_selectedGhostLabel);
     }
     
     // Button to select a different ghost
@@ -46,7 +48,7 @@ void PauseLayer::addRaceControls() {
         this,
         menu_selector(PauseLayer::onSelectGhost)
     );
-    selectBtn->setPosition(ccp(200, 115));
+    selectBtn->setPosition({200, 115});
     menu->addChild(selectBtn);
     
     this->addChild(menu);
@@ -55,13 +57,47 @@ void PauseLayer::addRaceControls() {
 void PauseLayer::updateSelectedGhostLabel() {
     auto* activeGhost = GhostManager::get().getActiveGhost();
     if (activeGhost && GhostManager::get().isRacing()) {
-        std::string text = fmt::format("Ghost: {}%", activeGhost->percentage);
-        if (m_selectedGhostLabel) {
-            m_selectedGhostLabel->setString(text.c_str());
+        std::string text = fmt::format("Ghost: {}%", static_cast<int>(activeGhost->percentage));
+        if (m_fields->m_selectedGhostLabel) {
+            m_fields->m_selectedGhostLabel->setString(text.c_str());
         } else {
-            m_selectedGhostLabel = CCLabelBMFont::create(text.c_str(), "bigFont.fnt");
+            m_fields->m_selectedGhostLabel = CCLabelBMFont::create(text.c_str(), "bigFont.fnt");
         }
-    } else if (m_selectedGhostLabel) {
+    } else if (m_fields->m_selectedGhostLabel) {
+        m_fields->m_selectedGhostLabel->setString("Ghost: none");
+    }
+}
+
+void PauseLayer::onToggleRace(CCObject* sender) {
+    bool newState = !GhostManager::get().isRacing();
+    GhostManager::get().setRacing(newState);
+    
+    if (m_fields->m_raceToggle) {
+        m_fields->m_raceToggle->toggle(newState);
+    }
+    
+    this->updateSelectedGhostLabel();
+}
+
+void PauseLayer::onSelectGhost(CCObject* sender) {
+    auto popup = GhostLibraryPopup::create(true);
+    popup->show();
+}
+
+void PauseLayer::onResume(CCObject* sender) {
+    if (GhostManager::get().isRacing()) {
+        auto* activeGhost = GhostManager::get().getActiveGhost();
+        if (activeGhost) {
+            auto* playLayer = PlayLayer::get();
+            if (playLayer) {
+                GhostRenderer::get().setGhost(activeGhost);
+                GhostRenderer::get().start(playLayer->m_gameState.m_time);
+            }
+        }
+    }
+    
+    PauseLayer::onResume(sender);
+}    } else if (m_selectedGhostLabel) {
         m_selectedGhostLabel->setString("Ghost: none");
     }
 }

daa91a2d47b34e5337afb9b5c8e8d203183cedba
hkr3d2 <151843832+hkr3d2@users.noreply.github.com>
2026-04-04 10:39:08 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,7 @@
 #include <Geode/Geode.hpp>
-#include "GhostManager.hpp"
-#include "GhostRenderer.hpp"
-#include "hooks/PlayLayerHook.hpp"
-#include "hooks/LevelInfoLayerHook.hpp"
-#include "hooks/PauseLayerHook.hpp"
 
 using namespace geode::prelude;
 
 $on_mod(Loaded) {
     log::info("Best Ghost Mod v1.0.0 loaded!");
-}
-
-$on_mod(Unloaded) {
-    log::info("Best Ghost Mod unloaded");
 }

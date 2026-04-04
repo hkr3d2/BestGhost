428f8f1fc8406a855dd4b695ca61f47304a52e7b
hkr3d2 <151843832+hkr3d2@users.noreply.github.com>
2026-04-04 10:44:14 +0200

    Update PlayLayerHook.cpp

--- src/hooks/PlayLayerHook.cpp
+++ src/hooks/PlayLayerHook.cpp
@@ -5,45 +5,10 @@ using namespace geode::prelude;
 
 class $modify(PlayLayer) {
     bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
-        log::info("Best Ghost: PlayLayer initialized for level: {}", level->m_levelName);
-        return PlayLayer::init(level, useReplay, dontCreateObjects);
+        if (!PlayLayer::init(level, useReplay, dontCreateObjects)) return false;
+        
+        log::info("Best Ghost: Entered level: {}", level->m_levelName);
+        
+        return true;
     }
-};    
-    // Record current frame for ghost saving
-    this->recordCurrentFrame();
-    
-    // Update ghost racing
-    this->updateGhostRacing(dt);
-}
-
-void PlayLayer::updateGhostRacing(float dt) {
-    if (!GhostManager::get().isRacing()) return;
-    if (!GhostRenderer::get().isActive()) return;
-    
-    float currentTime = m_gameState.m_time;
-    GhostRenderer::get().update(currentTime);
-}
-
-void PlayLayer::destroyPlayer(PlayerObject* player, GameObject* object) {
-    // Get current percentage before death
-    float percentage = m_gameState.m_percentage;
-    
-    // Stop recording and save if this was a new best
-    this->stopRecordingAttempt(percentage);
-    
-    PlayLayer::destroyPlayer(player, object);
-}
-
-void PlayLayer::resetLevel() {
-    // Start a fresh recording on reset
-    this->startRecordingAttempt();
-    
-    PlayLayer::resetLevel();
-}
-
-void PlayLayer::levelComplete() {
-    // Save the completion ghost (100%)
-    this->stopRecordingAttempt(100.0f);
-    
-    PlayLayer::levelComplete();
-}
+};

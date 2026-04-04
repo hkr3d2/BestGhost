d4e01cd5f3b2cd0bad0845ce89d40a2532e09a16
hkr3d2 <151843832+hkr3d2@users.noreply.github.com>
2026-04-04 10:35:47 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,6 @@ using namespace geode::prelude;
 
 $on_mod(Loaded) {
     log::info("Best Ghost Mod v1.0.0 loaded!");
-    
-    // Register settings
-    auto mod = Mod::get();
-    mod->addSetting<bool>("show-click-indicators", "Show Click Indicators", false);
-    mod->addSetting<bool>("show-overlay", "Show Comparison Overlay", false);
 }
 
 $on_mod(Unloaded) {

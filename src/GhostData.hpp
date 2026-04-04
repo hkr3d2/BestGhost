c034e7125c9c93f7bd6a1d48aeb9838e066d62d8
hkr3d2 <151843832+hkr3d2@users.noreply.github.com>
2026-04-04 10:39:55 +0200

    Update GhostData.hpp

--- src/GhostData.hpp
+++ src/GhostData.hpp
@@ -2,27 +2,21 @@
 #include <Geode/Geode.hpp>
 #include <vector>
 #include <string>
-#include <optional>
 
 using namespace geode::prelude;
 
 struct GhostFrame {
-    float x;
-    float y;
-    float rotation;
+    float x, y, rotation, timeOffset;
     bool isHolding;
     int gameMode;
-    float timeOffset;
 };
 
 struct GhostData {
     std::string levelID;
     float percentage;
     std::string timestamp;
-    int attemptsAtSave;
     std::vector<GhostFrame> frames;
     
     bool saveToFile(const std::string& path);
     static std::optional<GhostData> loadFromFile(const std::string& path);
-    static std::string getFilename(float percentage);
 };

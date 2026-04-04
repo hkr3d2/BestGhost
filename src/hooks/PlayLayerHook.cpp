2cc6174ddb1085aeff97fc20d9bc48605c739316
hkr3d2 <151843832+hkr3d2@users.noreply.github.com>
2026-04-04 10:40:48 +0200

    Update PlayLayerHook.cpp

--- src/hooks/PlayLayerHook.cpp
+++ src/hooks/PlayLayerHook.cpp
@@ -1,67 +1,14 @@
-#include "PlayLayerHook.hpp"
-#include "../GhostManager.hpp"
-#include "../GhostRenderer.hpp"
+#include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
 
-bool PlayLayer::init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
-    if (!PlayLayer::init(level, useReplay, dontCreateObjects)) return false;
-    
-    // Set current level in GhostManager
-    GhostManager::get().setCurrentLevel(std::to_string(level->m_levelID));
-    
-    // Start recording a new attempt
-    this->startRecordingAttempt();
-    
-    return true;
-}
+using namespace geode::prelude;
 
-void PlayLayer::startRecordingAttempt() {
-    m_fields->m_isRecording = true;
-    m_fields->m_lastRecordedTime = 0;
-    GhostManager::get().startRecording();
-    
-    // If racing mode is enabled, start the ghost renderer
-    if (GhostManager::get().isRacing()) {
-        auto* activeGhost = GhostManager::get().getActiveGhost();
-        if (activeGhost) {
-            GhostRenderer::get().setGhost(activeGhost);
-            GhostRenderer::get().start(m_gameState.m_time);
-        }
+class $modify(PlayLayer) {
+    bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
+        log::info("Best Ghost: PlayLayer initialized for level: {}", level->m_levelName);
+        return PlayLayer::init(level, useReplay, dontCreateObjects);
     }
-}
-
-void PlayLayer::stopRecordingAttempt(float percentage) {
-    if (!m_fields->m_isRecording) return;
-    m_fields->m_isRecording = false;
-    GhostManager::get().stopRecording(percentage);
-    
-    // Stop ghost renderer
-    GhostRenderer::get().stop();
-}
-
-void PlayLayer::recordCurrentFrame() {
-    if (!m_fields->m_isRecording) return;
-    if (!m_player1) return;
-    
-    float currentTime = m_gameState.m_time;
-    
-    // Record every frame (or throttle to every 1/60th of a second)
-    if (currentTime - m_fields->m_lastRecordedTime < 0.016f) return;
-    m_fields->m_lastRecordedTime = currentTime;
-    
-    PlayerObject* player = m_player1;
-    CCPoint pos = player->getPosition();
-    float rotation = player->getRotation();
-    bool isHolding = player->m_isHolding;
-    int gameMode = static_cast<int>(m_gameState.m_gameMode);
-    
-    GhostManager::get().recordFrame(
-        pos.x, pos.y, rotation, isHolding, gameMode, currentTime
-    );
-}
-
-void PlayLayer::update(float dt) {
-    PlayLayer::update(dt);
-    
+};    
     // Record current frame for ghost saving
     this->recordCurrentFrame();
     

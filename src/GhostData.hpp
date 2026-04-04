dd8ee3364745cb0daca27d6700408c287c2115b9
hkr3d2 <151843832+hkr3d2@users.noreply.github.com>
2026-04-04 17:22:55 +0200

    Create GhostData.hpp

---
+++ src/GhostData.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <Geode/Geode.hpp>
+#include <vector>
+
+using namespace geode::prelude;
+
+struct GhostFrame {
+    float x = 0;
+    float y = 0;
+    float rotation = 0;
+    float timeOffset = 0;
+    bool isHolding = false;
+    int gameMode = 0;
+};
+
+struct GhostData {
+    std::string levelID;
+    float percentage = 0;
+    std::vector<GhostFrame> frames;
+    
+    bool saveToFile(const std::string& path);
+    static std::optional<GhostData> loadFromFile(const std::string& path);
+};

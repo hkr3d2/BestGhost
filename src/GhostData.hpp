4ba8c61e39c37283511b1c4f96493ba459346b71
hkr3d2 <151843832+hkr3d2@users.noreply.github.com>
2026-04-04 10:17:41 +0200

    Refactor GhostFrame and GhostData structures

--- src/GhostData.hpp
+++ src/GhostData.hpp
@@ -2,17 +2,17 @@
 #include <Geode/Geode.hpp>
 #include <vector>
 #include <string>
-#include <ctime>
+#include <optional>
 
 using namespace geode::prelude;
 
 struct GhostFrame {
-    float x;                // position x
-    float y;                // position y
-    float rotation;         // player rotation
-    bool isHolding;         // is clicking/holding
-    int gameMode;           // 0=cube, 1=ship, 2=ball, 3=ufo, 4=wave, 5=robot, 6=spider
-    float timeOffset;       // time in seconds since start
+    float x;
+    float y;
+    float rotation;
+    bool isHolding;
+    int gameMode;
+    float timeOffset;
 };
 
 struct GhostData {
@@ -22,12 +22,7 @@ struct GhostData {
     int attemptsAtSave;
     std::vector<GhostFrame> frames;
     
-    // Save to file
     bool saveToFile(const std::string& path);
-    
-    // Load from file
     static std::optional<GhostData> loadFromFile(const std::string& path);
-    
-    // Get filename based on percentage
     static std::string getFilename(float percentage);
 };
